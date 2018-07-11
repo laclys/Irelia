d015b673d983b1e5f54236e71e06367b227145e1
Lac <laclys@126.com>
2018-07-11 22:52:50 +0800

    use immutable.js

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,17 @@
 import * as constant from './constants'
+import { fromJS } from 'immutable'
 
-const defaultState = {
+
+const defaultState = fromJS({
   focused: false
-}
+})
 
 export default (state = defaultState, action) => {
   if (action.type === constant.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    return state.set('focused', true)
   }
   if (action.type === constant.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false)
   }
-
   return state
 }
