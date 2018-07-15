57c194679789d10232634fd5ab605b2db0c982d0
Lac <laclys@126.com>
2018-07-15 16:54:01 +0800

    spin animation

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -43,8 +43,10 @@ class Header extends Component {
         >
           <SearchInfoTitle>
             热门搜索
-            <SearchInfoSwitch onClick={() => handlePageChange(page, totalPage)} >
-            <i className="iconfont">&#xe631;</i>
+            <SearchInfoSwitch
+              onClick={() => handlePageChange(page, totalPage, this.spinIcon )}
+            >
+              <i ref={(icon) => {this.spinIcon = icon}} className="iconfont spin">&#xe851;</i>
               换一批
             </SearchInfoSwitch>
           </SearchInfoTitle>
@@ -82,7 +84,7 @@ class Header extends Component {
               className={focused ? 'focused' : ''}
             ></NavSearch>
           </CSSTransition>
-          <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe631;</i>
+          <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe631;</i>
           { this.getListArea() }
         </SearchWrapper>
       </Nav>
@@ -123,7 +125,14 @@ const mapDispatchToprops = (dispatch) => {
     handleMouseLeave() {
       dispatch(actionCreators.mouseLeave())
     },
-    handlePageChange(page, totalPage) {
+    handlePageChange(page, totalPage, spin) {
+      let originAngle = spin.style.transform.replace(/[^0-9]/ig,'')
+      if (originAngle) {
+        originAngle = parseInt(originAngle, 10)
+      } else {
+        originAngle = 0
+      }
+      spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)'
       if (page < totalPage) {
         dispatch(actionCreators.changePage(page + 1))
       } else {
