99f1a0d46d583dee4ab529ff24b545e8b3080ce6
Lac <laclys@126.com>
2018-07-15 16:32:23 +0800

    update iconfont

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -43,7 +43,10 @@ class Header extends Component {
         >
           <SearchInfoTitle>
             热门搜索
-            <SearchInfoSwitch onClick={() => handlePageChange(page, totalPage)} >换一批</SearchInfoSwitch>
+            <SearchInfoSwitch onClick={() => handlePageChange(page, totalPage)} >
+            <i className="iconfont">&#xe631;</i>
+              换一批
+            </SearchInfoSwitch>
           </SearchInfoTitle>
           <SearchInfoList>
             { pageList }
