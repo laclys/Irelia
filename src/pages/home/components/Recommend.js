7330a0164a56358d0165908684ac7f6c00f5d772
Lac <laclys@126.com>
2018-07-17 22:31:28 +0800

    Home Comp split

---
+++ src/pages/home/components/Recommend.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class Recommennd extends Component {
+  render() {
+    return (
+      <div>Recommennd~</div>
+    )
+  }
+}
+
+export default Recommennd
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import Topic from './components/Topic'
+import List from './components/List'
+import Recommend from './components/Recommend'
+import Writer from './components/Writer'
 
+import { 
+  HomeWrapper,
+  HomeLeft,
+  HomeRight
+} from './style'
 
 class Home extends Component {
   render() {
     return (
-      <div>Home~</div>
+      <HomeWrapper>
+        <HomeLeft>
+          <img className="banner-img" src="https://upload.jianshu.io/admin_banners/web_images/4350/688c7b4c268fed979ea01f0b5ace56844bc6e51d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt="banner"/>
+          <Topic />
+          <List />
+        </HomeLeft>
+        <HomeRight>
+          <Recommend />
+          <Writer />
+        </HomeRight>
+      </HomeWrapper>
     )
   }
 }
