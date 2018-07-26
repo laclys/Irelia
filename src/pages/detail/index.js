938fbd21ad57585f553ccd6cdbf909cf313e4282
Lac <laclys@126.com>
2018-07-26 23:57:04 +0800

    add login style

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { DetailWrapper, Header, Content } from './style'
 import { actionCreators } from '../detail/store';
 
-class Detail extends Component {
+class Detail extends PureComponent {
 
   componentDidMount() {
     this.props.getDetailData(this.props.match.params.id)
